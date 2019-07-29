21d0953586e526e1dbfd681415d9091f58d545b6
Christophe van de Kerchove <christophe.vkerchove@fxinnovation.com>
2019-07-29 15:04:25 -0400

    Better tests

--- controls/default.rb
+++ controls/default.rb
@@ -9,32 +9,17 @@
 ###
 # Controls
 ###
-control 'resource_group_exists' do
+control 'resource_group' do
   impact 1.0
   title  'Checks the azure resource group'
   tag    'azurerm'
   tag    'resource_group'
-
-  only_if('module is disabled') do
-    input('enabled')
-  end
 
   describe azurerm_resource_groups.where(name: input('name')) do
     it { should exist }
-  end
-end
-
-control 'resource_group_not_exists' do
-  impact 1.0
-  title  'Checks the azure resource group'
-  tag    'azurerm'
-  tag    'resource_group'
-
-  only_if('module is enabled') do
-    false == input('enabled')
-  end
+  end if input('enabled')
 
   describe azurerm_resource_groups.where(name: input('name')) do
     it { should_not exist }
-  end
+  end unless input('enabled')
 end
