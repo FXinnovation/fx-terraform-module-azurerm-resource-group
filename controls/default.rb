b1c046ba5534b453bec422bf9c2a274d5e106122
Christophe van de Kerchove <christophe.vkerchove@fxinnovation.com>
2019-07-29 15:01:01 -0400

    Improve readability of tests

--- controls/default.rb
+++ controls/default.rb
@@ -9,14 +9,32 @@
 ###
 # Controls
 ###
-control 'resource_group' do
+control 'resource_group_exists' do
   impact 1.0
   title  'Checks the azure resource group'
   tag    'azurerm'
   tag    'resource_group'
 
+  only_if('module is disabled') do
+    input('enabled')
+  end
+
   describe azurerm_resource_groups.where(name: input('name')) do
-    it { should exist }     if input('enabled')
-    it { should_not exist } unless input('enabled')
+    it { should exist }
+  end
+end
+
+control 'resource_group_not_exists' do
+  impact 1.0
+  title  'Checks the azure resource group'
+  tag    'azurerm'
+  tag    'resource_group'
+
+  only_if('module is enabled') do
+    false == input('enabled')
+  end
+
+  describe azurerm_resource_groups.where(name: input('name')) do
+    it { should_not exist }
   end
 end
